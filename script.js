66f5c00561111df5b8643b5f88d2070a423cb0b2
Soufletty <mariareginagarcia201@gmail.com>
2026-02-04 19:06:19 -0300

    criação do botâo 'concluir'

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", function(event) {
     const texto = input.value;
     if (texto.trim() === "") {
         return;
-    }
+    };
      const novadiv = document.createElement("div");
      novadiv.classList.add("tarefcss");
      const tarefa = document.createElement("p");
@@ -15,11 +15,18 @@ form.addEventListener("submit", function(event) {
      const excluir = document.createElement("button");
      excluir.classList.add("btnexcl");
      excluir.textContent = "x";
+     const concluir = document.createElement("button")
+     concluir.textContent = "V"
+     concluir.classList.add("btncnclr")
+     concluir.addEventListener("click", function(event){
+        tarefa.classList.toggle("concluida")
+     })
      excluir.addEventListener ("click", function(event) {
     novadiv.remove();
                     }) ;
      novadiv.appendChild(tarefa);
      novadiv.appendChild(excluir);
+     novadiv.appendChild(concluir)
      lista.appendChild(novadiv);
     input.value = "";
 })
